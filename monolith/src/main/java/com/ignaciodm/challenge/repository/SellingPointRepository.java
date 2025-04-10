30d443d3a3c4aa9dc0f48c476e066b294ea28e30
NachoPC <ignaciodm93@gmail.com>
2025-04-10 16:28:27 -0300

    commit inicial con el monolito y el primer ms

---
+++ monolith/src/main/java/com/ignaciodm/challenge/repository/SellingPointRepository.java
@@ -0,0 +1,11 @@
+package com.ignaciodm.challenge.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.ignaciodm.challenge.models.SellingPoint;
+
+import reactor.core.publisher.Mono;
+
+public interface SellingPointRepository extends ReactiveMongoRepository<SellingPoint, Integer> {
+	Mono<SellingPoint> findById(Integer id);
+}
---
+++ selling-points/src/main/java/com/challenge/ms/selling_points/App.java
@@ -0,0 +1,10 @@
+package com.challenge.ms.selling_points;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
